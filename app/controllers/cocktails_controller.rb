d553fc1cb1ca4fefba05bc9935512c2f13ca58b2
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2020-11-12 15:07:15 +0000

    added feature to add cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,4 +6,20 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
   end
+
+  def create
+    @ingredients = Ingredient.all
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail.id)
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  private
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
