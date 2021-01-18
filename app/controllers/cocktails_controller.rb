9a0477c33f58e9add80d77ad249f66bfc2e3ac55
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2021-01-18 14:19:41 +0000

    changed the VH of the new cocktail container so that the footer didnt snap to the immediate bottom of the container

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,6 +10,10 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.photo.nil?
+      @cocktail.photo.key = "https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/1/8/6/0/230681-6-eng-GB/IB3-Limited-SIC-Pharma-April-20142_news_large.jpg"
+    end
     if @cocktail.save
       redirect_to cocktail_path(@cocktail.id)
     else
